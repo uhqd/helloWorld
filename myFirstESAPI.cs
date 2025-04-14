b2d3642e05eed7f6413abe0262b3691e592e4d84
uhqd <uhqd75@gmail.com>
2025-04-14 12:21:13 +0200

    Ajoutez des fichiers projet.

---
+++ myFirstESAPI.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+// TODO: Uncomment the following line if the script requires write access.
+// [assembly: ESAPIScript(IsWriteable = true)]
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public Script()
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Execute(VMS.TPS.Common.Model.API.ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
+        {
+            MessageBox.Show("HELLO WORLD !!!");
+
+
+
+            if (context.PlanSetup != null)
+            {
+                MessageBox.Show("The following patient is open  " + context.Patient.Id);
+            }
+
+
+        }
+    }
+}
